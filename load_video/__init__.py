bc03f08997a90f2387f693c134afb25e27543368
victor-iyiola <javafolabi@gmail.com>
2017-09-14 15:23:01 +0100

    Added .gitignore

---
+++ load_video/__init__.py
@@ -0,0 +1,7 @@
+"""
+  @author Victor I. Afolabi
+  A.I. Engineer & Software developer
+  javafolabi@gmail.com
+  Created on 14 September, 2017 @ 3:21 PM.
+  Copyright © 2017. Victor. All rights reserved.
+"""
