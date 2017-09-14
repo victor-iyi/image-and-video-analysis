c8ed7a23b619a6ad5974d659f04356b12ec90730
victor-iyiola <javafolabi@gmail.com>
2017-09-14 15:31:34 +0100

    Loading video from webcam

--- load_video/__init__.py
+++ load_video/__init__.py
@@ -5,3 +5,23 @@
   Created on 14 September, 2017 @ 3:21 PM.
   Copyright © 2017. Victor. All rights reserved.
 """
+
+import cv2
+
+
+def main():
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        _, frame = cap.read()
+        cv2.imshow('Frame', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
