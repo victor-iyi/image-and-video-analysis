5ae7285b175091f05ae100966bcfc94fa13c2ddf
victor-iyiola <javafolabi@gmail.com>
2017-09-15 06:22:47 +0100

    loading from webcam

--- load/video.py
+++ load/video.py
@@ -5,3 +5,26 @@
   Created on 15 September, 2017 @ 5:44 AM.
   Copyright © 2017. Victor. All rights reserved.
 """
+
+import cv2
+
+
+def webcam(source=0, title="webcam"):
+    capture = cv2.VideoCapture(source)
+    status, img = capture.read()
+
+    print('Press <SPACE> to exit...')
+
+    while status:
+        status, img = capture.read()
+        cv2.imshow(title, img)
+        if cv2.waitKey(1) & 0xFF == ord(' '):
+            break
+
+    capture.release()
+    cv2.destroyAllWindows()
+    return True
+
+
+if __name__ == '__main__':
+    webcam()
