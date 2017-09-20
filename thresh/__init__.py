e48125fb0c9d2723707e9b0090e430be46398542
victor-iyiola <javafolabi@gmail.com>
2017-09-20 15:52:02 +0100

    Threshold package

---
+++ thresh/__init__.py
@@ -0,0 +1,7 @@
+"""
+  @author Victor I. Afolabi
+  A.I. Engineer & Software developer
+  javafolabi@gmail.com
+  Created on 20 September, 2017 @ 3:51 PM.
+  Copyright © 2017. Victor. All rights reserved.
+"""
