04d38ed2dfa3807fa63fcae8d5affa5b384b5487
victor-iyiola <javafolabi@gmail.com>
2017-10-03 21:19:51 +0100

    Documentation for threshold module

--- thresh/threshold.py
+++ thresh/threshold.py
@@ -8,24 +8,29 @@
 import cv2
 
 
-def binary(img, thresh_val, maxval, thresh_type=cv2.THRESH_BINARY, grayscale=True,
-           display=False):
+def binary(img, thresh_val, maxval, thresh_type=cv2.THRESH_BINARY,
+           grayscale=True, display=False):
     """
-    Binary threshold.
+    Binary threshold
 
     The function applies fixed-level thresholding to a multiple-channel array.
-    The function is typically used to get a bi-level (binary) image out of a grayscale
-    image ( cv::compare could be also used for this purpose) or for removing a noise,
-    that is, filtering out pixels with too small or too large values. There are several
-    types of thresholding supported by the function. They are determined by type parameter.
+    The function is typically used to get a bi-level (binary) image out of a
+    grayscale image ( cv::compare could be also used for this purpose) or for
+    removing a noise, that is, filtering out pixels with too small or too large
+    values. There are several types of thresholding supported by the function.
+    They are determined by type parameter.
 
-    Also, the special values cv::THRESH_OTSU or cv::THRESH_TRIANGLE may be combined with one of the
-    above values. In these cases, the function determines the optimal threshold value using the Otsu's
-    or Triangle algorithm and uses it instead of the specified thresh . The function returns the
-    computed threshold value. Currently, the Otsu's and Triangle methods are implemented only for 8-bit
-    images.
+    Also, the special values cv::THRESH_OTSU or cv::THRESH_TRIANGLE may be
+    combined with one of the above values. In these cases, the function
+    determines the optimal threshold value using the Otsu's or Triangle
+    algorithm and uses it instead of the specified thresh . The function
+    returns the computed threshold value. Currently, the Otsu's and Triangle
+    methods are implemented only for 8-bit images.
 
-    @note Input image should be single channel only in case of CV_THRESH_OTSU or CV_THRESH_TRIANGLE flags
+    @note Input image should be single channel only in case of CV_THRESH_OTSU
+    or CV_THRESH_TRIANGLE flags.
+
+    
     :param img:
             input array (multiple-channel, 8-bit or 32-bit floating point).
     :param thresh_val:
@@ -54,26 +59,41 @@ def adaptive(img, maxval, method=cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
              thresh_type=cv2.THRESH_BINARY, block_size=13,
              C=19, display=False):
     """
-    Adaptive threshold.
+    Adaptive threshold
 
-    The function transforms a grayscale image to a binary image according to the formulae:
-        The function can process the image in-place.
+    @brief Applies an adaptive threshold to an array.
+
+    The function transforms a grayscale image to a binary image according
+    to the formulae:
+    =>   **THRESH_BINARY**
+            \f[dst(x,y) =  \fork{\texttt{maxValue}}{if \(src(x,y) > T(x,y)\)}
+                                {0}{otherwise}\f]
+    =>   **THRESH_BINARY_INV**
+            \f[dst(x,y) =  \fork{0}{if \(src(x,y) > T(x,y)\)}
+                                {\texttt{maxValue}}{otherwise}\f]
+            where \f$T(x,y)\f$ is a threshold calculated individually for
+                                each pixel (see adaptiveMethod parameter).
+
+    The function can process the image in-place.
 
     :param img:
             Source 8-bit single-channel image.
     :param maxval:
-            Non-zero value assigned to the pixels for which the condition is satisfied
+            Non-zero value assigned to the pixels for which the condition is
+            satisfied
     :param method:
-            Adaptive thresholding algorithm to use, see cv::AdaptiveThresholdTypes
+            Adaptive thresholding algorithm to use.
+            see cv::AdaptiveThresholdTypes
     :param thresh_type:
-            Thresholding type that must be either THRESH_BINARY or THRESH_BINARY_INV,
-            see cv::ThresholdTypes.
+            Thresholding type that must be either THRESH_BINARY or
+            THRESH_BINARY_INV, see cv::ThresholdTypes.
     :param block_size:
-            Size of a pixel neighborhood that is used to calculate a threshold value for the
-            pixel: 3, 5, 7, and so on.
+            Size of a pixel neighborhood that is used to calculate a threshold
+            value for the pixel: 3, 5, 7, and so on.
     :param C:
-            Constant subtracted from the mean or weighted mean (see the details below). Normally, it
-            is positive but may be zero or negative as well.
+            Constant subtracted from the mean or weighted mean
+            (see the details below). Normally, it is positive but may be zero
+            or negative as well.
     :param display:
             If set to true the thresheld image is displayed
 
