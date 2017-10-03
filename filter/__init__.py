e21900c3b9a816f9881684783560aef30cdea30a
victor-iyiola <javafolabi@gmail.com>
2017-10-03 21:32:16 +0100

    Filter package

---
+++ filter/__init__.py
@@ -0,0 +1,17 @@
+"""
+  @author Victor I. Afolabi
+  A.I. Engineer & Software developer
+  javafolabi@gmail.com
+  Created on 03 October, 2017 @ 9:29 PM.
+  Copyright © 2017. Victor. All rights reserved.
+"""
+
+
+"""
+The :mod:`thresh` module implements generalized threshold functions. It
+includes binary and adaptive modes of threshold
+"""
+
+__all__ = [
+
+]
