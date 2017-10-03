7715b9a35dc7e1d07cd0d32556ac142c1e6c0c9c
victor-iyiola <javafolabi@gmail.com>
2017-10-03 20:48:21 +0100

    Adaptive Threshold

--- thresh/threshold.py
+++ thresh/threshold.py
@@ -8,19 +8,34 @@
 import cv2
 
 
-def binary(img, minval, maxval, grayscale=True):
+def binary(img, minval, maxval, thresh_type=cv2.THRESH_BINARY, grayscale=True, display=False):
     if grayscale:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    _, image_thresh = cv2.threshold(img, minval, maxval, cv2.THRESH_BINARY)
-    return image_thresh
+    _, img_thresh = cv2.threshold(img, minval, maxval, thresh_type)
+    if display:
+        cv2.imshow('Binary Threshold', img_thresh)
+    return img_thresh
+
+
+def adaptive(img, maxval, method=cv2.ADAPTIVE_THRESH_GAUSSIAN_C, thresh_type=cv2.THRESH_BINARY, block_size=13, C=19, display=False):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_thresh = cv2.adaptiveThreshold(img, maxval, method, thresh_type, block_size, C)
+    if display:
+        cv2.imshow('Adaptive Threshold', img_thresh)
+    return img_thresh
+
+
+def main():
+    img = cv2.imread('../data/images/3.jpg')
+    cv2.imshow('Original', img)
+    # Binary threshold
+    binary(img, 150, 255, grayscale=False, display=True)
+    # Adaptive threshold
+    adaptive(img, 255, display=True)
+    
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    img = cv2.imread('../data/images/3.jpg')
-    cv2.imshow('img', img)
-    img = binary(img, 150, 255)
-
-    cv2.imshow('Binary threshold', img)
-
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    main()
